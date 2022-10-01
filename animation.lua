96f39275cfd4c02a9fa01eff618b8e74bdf8fadb
Loc@lTrash <105620888+Local-Trash@users.noreply.github.com>
2022-10-01 16:11:05 -0700

    Add files via upload

---
+++ animation.lua
@@ -0,0 +1,15 @@
+--this is where you put your frames
+frame = {
+    ">----\n",
+    "=>---\n",
+    "==>--\n",
+    "===>-\n",
+    "====>\n",
+    "=====\n"
+}
+
+--This is the amount of frames
+frame.frames = 6
+
+--The seconds between each frame 
+sec = 0.1
